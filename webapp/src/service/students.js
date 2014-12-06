d166ac28710fff1e6db2b5d7f2ed0a2740e485af
reaperes <emerald105@hanmail.net>
2014-12-07 01:22:30 +0900

    add students service

--- webapp/src/service/students.js
+++ webapp/src/service/students.js
@@ -7,20 +7,45 @@ var database = require(path.join(SRC_ROOT, 'modules/database.js'));
 
 /**
  * Get random student's profiles.
- * @param {Number} n
+ * @param {Function} callback Callback function returns student's profiles.
+ * [{
+ *    nameKor: String,
+ *    nameEng: String,
+ *    imgName: String,
+ *    title: String,
+ *    vision: String,
+ *    movieUrl: String
+ *  },
+ *  ...
+ * ]
+ * @param {Number} num number of student's profiles
  */
-exports.getRndProfiles = function (n) {
-  // TODO
+exports.getRndProfiles = function (num, callback) {
+  var sampleObj = {
+    nameKor: '김다혜',
+    nameEng: 'KimDH',
+    imgName: 'test.jpg',
+    title: '나무처럼 살고픈 김다혜',
+    vision: '소프트웨어를 통해 공동체의 가치를 이루고 싶습니다.',
+    movieUrl: 'http://www.youtube.com/v/7vofKraOJcU'
+  };
+
+  var arr = [];
+  for (var i=0; i<num; i++)
+    arr.push(sampleObj);
+
+  callback(arr);
 };
 
 /**
- * Counting rows in Students table
- * @param {function} callback Callbac
+ * Counting rows in Students table.
+ * @param {function} callback Callback
  * @return {Number} count of all students
  */
 exports.getMaxStudents = function (callback) {
-  database.query('SELECT MAX(stu_id) - MIN(stu_id) + 1 AS num FROM Students', function (err, rows, fields) {
-    if (err) throw err;
-    callback(rows[0].num);
+  database.query('SELECT MAX(stu_id) - MIN(stu_id) + 1 AS num FROM Students'
+    , function (err, rows, fields) {
+      if (err) throw err;
+      callback(rows[0].num);
   });
 };
