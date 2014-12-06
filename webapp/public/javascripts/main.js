f8c15d4f83163c4aa3c1cec7a7a99a49ace4c37f
heesu <heesu@nhnnext.org>
2014-12-06 20:33:06 +0900

    change backgroundcolor in js

--- webapp/public/javascripts/main.js
+++ webapp/public/javascripts/main.js
@@ -1,7 +1,7 @@
 $(".card").hover(function() {
     //alert("hi");
-    $(this).find(".info").css("display","block");
+    $(this).find(".info").css({display:'block', backgroundColor:'white'});
 }, function() {
     //alert("hello");
-    $(this).find(".info").css("display","none");
+    $(this).find(".info").css({display:'block'});
 });
