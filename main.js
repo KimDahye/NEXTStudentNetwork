c9323d3eb15e1aee761853b0ae5b394697b74cdc
heesu <heesu@nhnnext.org>
2014-12-06 20:18:25 +0900

    change directory structure

--- main.js
+++
@@ -1,7 +0,0 @@
-$(".card").hover(function() {
-    //alert("hi");
-    $(this).find(".info").css("display","block");
-}, function() {
-    //alert("hello");
-    $(this).find(".info").css("display","none");
-});
--- webapp/public/javascripts/main.js
+++ webapp/public/javascripts/main.js
@@ -1,1 +1,7 @@
-// javascript
+$(".card").hover(function() {
+    //alert("hi");
+    $(this).find(".info").css("display","block");
+}, function() {
+    //alert("hello");
+    $(this).find(".info").css("display","none");
+});
