8da8f05f54d83108416c61f7e9b7959987056bdd
heesu <heesu@nhnnext.org>
2014-12-06 23:05:07 +0900

    change random bg color

--- webapp/public/javascripts/main.js
+++ webapp/public/javascripts/main.js
@@ -1,7 +1,22 @@
 $(".card").hover(function() {
     //alert("hi");
-    $(this).find(".info").css({display:'block', backgroundColor:'white'});
+    $(this).find(".info").css({display:'block', backgroundColor:getRandomBGcolor()});
 }, function() {
     //alert("hello");
     $(this).find(".info").css({display:'none'});
 });
+
+
+function getRandomBGcolor() {
+	var bgcArray = 
+	[
+	'rgb(0, 210, 103)',
+	'rgb(11, 110, 232)',
+	'rgb(110, 232, 255)',
+	'rgb(190, 119, 90)',
+	'rgb(255, 111, 65)',
+	'rgb(255, 225, 14)'
+	];
+	var index = Math.floor((Math.random() * bgcArray.length));
+	return bgcArray[index];
+}
