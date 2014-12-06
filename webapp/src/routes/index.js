ee8fd7ae0863474f3c989c2d49cbfdfa5b15f8ed
reaperes <emerald105@hanmail.net>
2014-12-07 00:40:08 +0900

    update render page

--- webapp/src/routes/index.js
+++ webapp/src/routes/index.js
@@ -3,7 +3,7 @@ var router = require('express').Router();
 // Router starts '/'
 
 router.get('/', function(req, res) {
-  res.status(200).send('Hello world!');
+  res.status(200).render('main.html');
 });
 
 module.exports = router;
