1d344be50d048176c0eb418cb8edf3a861bbccf9
heesu <heesu@nhnnext.org>
2014-12-07 19:59:27 +0900

    embed youtube

--- webapp/public/javascripts/video.js
+++ webapp/public/javascripts/video.js
@@ -18,6 +18,13 @@ function videoModalOn() {
 		return;
 	}
 
+	var ytAPIscript = document.createElement('iframe');
+	ytAPIscript.src="http://www.youtube.com/embed/"+hash.substr(1);
+	ytAPIscript.id="video";
+	ytAPIscript.width=$(window).width()*0.8;
+	ytAPIscript.height=$(window).height()*0.8;
+	$("#videoModal").prepend(ytAPIscript);
+
 	$("#mask").show();
 	var maskZindex = parseInt($("#mask").css('zIndex'));
 	$("#videoModal").css('zIndex', maskZindex+1);
