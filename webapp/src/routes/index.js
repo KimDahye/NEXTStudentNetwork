d52e16eb0e051c258a414ced211a20219a0fca41
heesu <heesu@nhnnext.org>
2014-12-09 16:49:55 +0900

    ejs get data from server, not hard coding

--- webapp/src/routes/index.js
+++ webapp/src/routes/index.js
@@ -1,25 +1,13 @@
 var router = require('express').Router();
+var path = require('path');
+var studentsService = require(path.join(SRC_ROOT, 'service/students.js'));
 
 // Router starts '/'
 
 router.get('/', function(req, res) {
-  res.status(200).render('main.html', {"data":[
-  	{"nameKor":"김다혜",
-  	"nameEng":"KimDaHye",
-  	"majorKor":"웹 서버 전공",
-  	"majorEng":"Web server track",
-  	"imgName":"http://placehold.it/200x200",
-  	"title":"나무처럼 살고픈 김다혜",
-  	"vision":"소프트웨어를 통해 공동체의 가치를 이루고 싶습니다.",
-  	"movieUrl":"Kd8LDnW9t-k"},
-  	{"nameKor":"정희수",
-  	"nameEng":"Jung Heesu",
-  	"majorKor":"웹 UI 전공",
-  	"majorEng":"Web UI track",
-  	"imgName":"http://placehold.it/200x200",
-  	"title":"테스트용 정희수",
-  	"vision":"뉸뉴냔냐",
-  	"movieUrl":"tHiUZfgf1WQ"}]});
+  studentsService.getRndProfiles(4, function (data) {
+    res.status(200).render('main.html', {"data":data});
+  });
 });
 
 module.exports = router;
