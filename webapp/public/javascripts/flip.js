f42b0022fb3908763bd78624dd58af2e9518aed2
heesu <heesu@nhnnext.org>
2014-12-10 02:06:13 +0900

    change info after animation

--- webapp/public/javascripts/flip.js
+++ webapp/public/javascripts/flip.js
@@ -1,9 +1,29 @@
 var leastNum = 2, requestNum = 24,
 	changeInterval = 5000,
-	data=[];
+	data=[],
+	transitionEvent = getTransitionEvent();
 
 checkDataRemaining();
 window.setInterval(changeStudent, changeInterval);
+
+function getTransitionEvent(){
+	var el = document.createElement('fakeelement'),
+	transitions = {
+		'transition':'transitionend',
+		'OTransition':'oTransitionEnd',
+		'MozTransition':'transitionend',
+		'WebkitTransition':'webkitTransitionEnd'
+    };
+
+	for(var t in transitions) {
+		if( el.style[t] !== undefined ){
+			return transitions[t];
+		}
+	}
+
+	console.error("no transitionEvent!");
+	// http://davidwalsh.name/css-animation-callback
+}
 
 function requestMoreStudent() {
 	console.log("@requestMoreStudent");
@@ -32,17 +52,29 @@ function changeStudent() {
 	console.log(index);
 	var card = cards[index];
 	var newData = data.shift();
-	flip(card, newData.imgName);
+	flip(card, newData.imgName, function() {
+		$(this).removeClass("flipper")
+			.parent().attr("href", newData.movieUrl)
+			.find(".front").remove();
+		$(this).find(".back")
+			.removeClass("back");
+		$("<div></div>").insertBefore($(".profile", this))
+			.addClass("info")
+			.append("<div class='name'>"+newData.nameKor+"</div>")
+			.append("<div class='major'>"+newData.majorKor+"</div>")
+			.append("<div class='motto'>"+newData.vision+"</div>");
+	});
 }
 
-function flip(card, imgsrc) {
+function flip(card, imgsrc, addInfo) {
 	$(card).find(".info").remove();
 	$(card).find(".profile").addClass("front");
-	console.log($(card).find(".front"));
+	// console.log($(card).find(".front"));
 	$("<img />").insertAfter($(card).find(".front"))
-		.addClass("back")
+		.addClass("profile back")
 		.attr("src", imgsrc);
-	$(card).addClass("flipper");
+	$(card).addClass("flipper")
+		.on(transitionEvent, addInfo);
 }
 
 function checkDataRemaining() {
