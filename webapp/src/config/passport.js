d5406b2b6216f06dfd1231f37176868e0c323b8b
Hoyoung Jung <honux77@gmail.com>
2015-12-31 21:44:18 +0900

    login success

--- webapp/src/config/passport.js
+++ webapp/src/config/passport.js
@@ -97,18 +97,18 @@ module.exports = function(passport) {
 
         // find a user whose email is the same as the forms email
         // we are checking to see if the user trying to login already exists
-        User.findOne({ 'local.email' :  email }, function(err, user) {
+        User.findOne({ 'email' :  email }, function(err, user) {
             // if there are any errors, return the error before anything else
             if (err)
                 return done(err);
 
             // if no user is found, return the message
             if (!user)
-                return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
+                return done(null, false, req.flash('loginMessage', '해당 유저가 없습니다.')); // req.flash is the way to set flashdata using connect-flash
 
             // if the user is found but the password is wrong
             if (!user.validPassword(password))
-                return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
+                return done(null, false, req.flash('loginMessage', '패스워드가 틀렸습니다.')); // create the loginMessage and save it to session as flashdata
 
             // all is well, return successful user
             return done(null, user);
--- webapp/src/routes/routes.js
+++ webapp/src/routes/routes.js
@@ -7,7 +7,11 @@ module.exports = function(app, passport) {
     });
 
     // process the login form
-    // app.post('/login', do all our passport stuff here);
+    app.post('/login', passport.authenticate('local-login', {
+    successRedirect : '/profile', // redirect to the secure profile section
+    failureRedirect : '/', // redirect back to the signup page if there is an error
+    failureFlash : true // allow flash messages
+}));
 
     // =====================================
     // SIGNUP ==============================
