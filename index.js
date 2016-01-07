0305323af31edc7d258f9ad0eebe43b655eeec5d
pjc02478 <pjc0247@gmail.com>
2016-01-07 16:41:41 +0900

    change default port to 8080

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('./global.js');
 var info = require('debug')('info:www');
 var app = require('./webapp/src/app.js');
 
-app.set('port', 3000);
+app.set('port', 8080);
 
 var server = app.listen(app.get('port'), function() {
   info('Express server listening on port ' + server.address().port);
