cfe2ce4a813cb0f6d3b83386a2d31dc3431481a3
pjc02478 <pjc0247@gmail.com>
2016-01-07 17:19:55 +0900

    configure port from ENVVAR instead const literal

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ require('./global.js');
 var info = require('debug')('info:www');
 var app = require('./webapp/src/app.js');
 
-app.set('port', 8080);
+var port = process.env.PORT || 8080;
+app.set('port', port);
 
 var server = app.listen(app.get('port'), function() {
   info('Express server listening on port ' + server.address().port);
